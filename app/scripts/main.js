bc8bd668bb2d2a2912379918fcae3d69fb5c3a84
Maxim <maxim_ek@mail.ru>
2016-09-08 03:32:04 +0500

    second page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  $('.menu_btn').click(function() {
+    if ($('#nav-icon').attr('class') === 'open') {
+      $('.menu_overlay').fadeOut();
+    } else {
+      $('.menu_overlay').fadeIn();
+    }
+    $('#nav-icon').toggleClass('open');
+  });
+
+  $('.parent_item').click(function(e) {
+      if (e.target.className !== 'parent_item') return false;
+
+      var inner = $('.inner_menu');
+
+      if (inner.attr('class').indexOf('open') !== -1) {
+        inner.slideUp().toggleClass('open')
+      } else {
+        inner.slideDown().toggleClass('open');
+      }
+    });
+
+
+    $("#main-slider").owlCarousel({
+      navigationText: [
+      "<span class='control-wrapper'><img class='slider-controls' src='images/arrow_left.png'></span>",
+      "<span  class='control-wrapper'><img class='slider-controls' src='images/arrow_right.png'></span>"],
+      navigation : true,
+      dots: true,
+      singleItem:true
+    });
+
+});
